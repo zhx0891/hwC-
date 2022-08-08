cbab78fed2e6c97881091049e5798cd23a44cafe
zhukovv <northbug@yandex.ru>
2022-08-08 15:50:24 +0500

    решена первая задача  первого урока

--- example1/Program.cs
+++ example1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int max = 0, min =0;
+int value1 = Convert.ToInt32(Console. ReadLine());
+int value2 = Convert.ToInt32(Console .ReadLine());
+if(value1 > value2) 
+{
+max = value1;
+min = value2;
+}
+else
+{
+max = value2;
+min = value1;   
+}
+
+ Console .WriteLine($"Максимальное число {max}  минимальное {min} ");
