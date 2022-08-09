c70b1771d3fa9c85ad672a606581ad68838f3770
zhukovv <northbug@yandex.ru>
2022-08-09 10:50:28 +0500

    добавлено решение второй задачи для первой работы

---
+++ example2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console .WriteLine("Введи три числа");
+int a = Convert .ToInt32(Console .ReadLine());
+int b = Convert .ToInt32(Console .ReadLine());
+int c = Convert .ToInt32(Console .ReadLine());
+int max =0;
+if(a > b) 
+{
+    max = a; 
+}
+else if (b > c)
+{
+    max = b;
+}
+else 
+{
+    max = c;     
+}
+Console .WriteLine($"Максимальное число {max}");
