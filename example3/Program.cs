24bf851aa1d0e3ea15092080cbaac58bbe9b82de
zhukovv <northbug@yandex.ru>
2022-08-09 11:35:33 +0500

    решено третье задание первой работы

--- example3/Program.cs
+++ example3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console .WriteLine("Введите проверяемое число");
+int a = Convert .ToInt32 (Console .ReadLine ());
+if (a%2>0)
+{
+    Console .WriteLine ("Число не чётное");
+}
+else
+{
+    Console .WriteLine ("Число чётное"); 
+}
