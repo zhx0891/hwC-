285f892257dd7fc65eae841d269293228498b7f1
zhukovv <northbug@yandex.ru>
2022-08-14 22:27:13 +0500

    решено less2exp2

---
+++ less2exp2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите число");
+string? a = Console .ReadLine();
+Console.WriteLine(a);
+
+if (a .Length < 3)
+{
+    Console.WriteLine("в этом числе нет третьей цифры");
+}
+else 
+{
+    Console.WriteLine(a[2]);
+}
