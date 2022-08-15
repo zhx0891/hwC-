e27c3f45b99722a8161ee3940245671f96aa601f
zhukovv <northbug@yandex.ru>
2022-08-15 23:07:53 +0500

    c 21 до 23

--- less3exp1/Program.cs
+++ less3exp1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("?");
+int a = Convert .ToInt32(Console.ReadLine());
+int digit =10; 
+int indexes  = 1;
+
+while (a > digit )
+{
+    indexes ++;
+    digit = digit * 10;
+}
+System.Console.WriteLine(indexes + " "+ digit  );
