ce30c387ad14e6e75e30ec777913021ad02b940c
zhukovv <northbug@yandex.ru>
2022-08-17 21:48:00 +0500

    решена трерья задача

---
+++ less3exp3/MyLib.cs
@@ -0,0 +1,49 @@
+public static class MyLib
+{
+    public static int numToStep(int n, int step)
+    {
+        int incr = n;
+
+        for (int h = 1; h != step; h++)
+        {
+            n = n * incr;
+        }
+        return n;
+    }
+
+    public static int getIndexes(int num)
+    {
+        int digit = 10;
+        int stp = 1;
+
+
+        while (num > digit)
+        {
+            stp++;
+            digit = numToStep(10, stp);
+        }
+        return stp;
+    }
+
+    public static int[] intToArray(int num, int count)
+    {
+        int i, x = 0, y=0;
+        int[] arr = new int[count];
+        for (i = (count - 1); i >= 0; i--)
+        {
+            x++;
+
+            if (i == arr.Length - 1)
+            {
+                 arr[i] = num % (numToStep(10, x));
+             }
+            else
+            {
+                y++;
+                arr[i] = (num / (numToStep(10, y)))%10;
+            }
+            System.Console.WriteLine(arr[i] + " " + i);
+        }
+        return arr;
+    }
+}
--- less3exp3/Program.cs
+++ less3exp3/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//  public static MyLib 
+System.Console.WriteLine(" ? ");
+int N = Convert .ToInt32(System.Console.ReadLine());
+int [] array = new int [N+1];
+for(int i =1; i <= N; i++)
+{
+  array [i-1] =  MyLib .numToStep(i, 3);
+  System.Console.Write(" " + array [i-1]);
+}
