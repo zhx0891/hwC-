a20ced8b98af494fad40d58419d71a05d46bdca8
zhukovv <northbug@yandex.ru>
2022-08-18 21:33:47 +0500

    !!!!

--- less3exp1/Program.cs
+++ less3exp1/Program.cs
@@ -1,4 +1,20 @@
-﻿// Console.WriteLine("?");
-int a = 123456; //
+﻿ 
+// Console.WriteLine("?");
+// int a =Convert .ToInt32(Console .ReadLine());
+int a = 123456;
+
 int ind = MyLib.getIndexes(a);
-MyLib.intToArray(a, ind);
+int [] NumArray = new int [ind];
+NumArray = MyLib.intToArray(a, ind);
+for(int i = ind/2; i>=0; i--)
+{
+    if (NumArray [i]!=NumArray[NumArray .Length ^i])
+{
+    System.Console.WriteLine("это не палиндром ");
+}
+else 
+{
+    System.Console.WriteLine(" это палиндром");
+}
+}
+
