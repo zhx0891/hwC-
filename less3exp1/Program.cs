bdc229ebf84a5b3967eb26c64b194bd29317029c
zhukovv <northbug@yandex.ru>
2022-08-18 09:03:24 +0500

    первая задача

--- less3exp1/Program.cs
+++ less3exp1/Program.cs
@@ -1,4 +1,15 @@
 ﻿// Console.WriteLine("?");
-int a = 123456; //
+int a = 111111; //
+// int count = 0;
 int ind = MyLib.getIndexes(a);
-MyLib.intToArray(a, ind);
+int [] arrNum = new int [ind];
+arrNum = MyLib.intToArray(a, ind);
+for (int i = arrNum.Length/2; i!=0; i--)
+{
+    if(arrNum [i]!=arrNum[arrNum.Length^i])
+    {
+        System.Console.WriteLine(" it's not a palindrome ");
+        break ;
+    }
+    System.Console.WriteLine(" this is palindrome ");
+}
