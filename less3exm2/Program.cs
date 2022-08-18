40ebb5cfb30eb7353b50428b6b7f7ffd7206a495
zhukovv <northbug@yandex.ru>
2022-08-18 15:46:38 +0500

    Решена вторая задача

--- less3exm2/Program.cs
+++ less3exm2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Console.WriteLine("введите координаты первой точки через пробел");
+// string p1 = Console.ReadLine();
+
+// Console.WriteLine("введите координаты второй точки через пробел");
+// string p2 = Console.ReadLine();
+// System.Console.WriteLine(point1[1]);
+double [] p1 = {3, 5, 5};
+double [] p2 = {4, 6, 1};
+System.Console.WriteLine(Math .Sqrt((Math .Pow ((p2[0]-p1[0]),2))+(Math .Pow ((p2[1]-p1[1]),2))+(Math .Pow ((p2[2]-p1[2]),2))));
+
+
+
+
