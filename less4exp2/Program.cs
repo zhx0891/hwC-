6edc34a1edd56b1d1ee0bc31cc32c8b5edf918e4
zhukovv <northbug@yandex.ru>
2022-08-19 16:07:58 +0500

    решена вторая задача

---
+++ less4exp2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("?");
+int a = Convert .ToInt32 (Console. ReadLine()); 
+int index = MyLib.getIndexes (a); 
+int [] num = new int [index]; 
+num = MyLib.intToArray(a, index);
+System.Console.WriteLine(MyLib .giveMeSummArr(num ));
