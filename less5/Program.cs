b89d4608380e34f6ee4e71b0c72bcd9ad6f6ae6a
zhukovv <northbug@yandex.ru>
2022-08-23 13:11:40 +0500

    solved second task

--- less5/Program.cs
+++ less5/Program.cs
@@ -1,13 +1,23 @@
 ﻿
-// Первая  задача.
-int multiple = 0;
-int noMultiple = 0;
-int[] expOne = new int[24];
-expOne = MyLib.giveFullIntArray(3, 24);
-for (int i = 0; i < expOne.Length; i++)
+System.Console.WriteLine("Первая задача.Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве. Нажмите ENTER ");
+System.Console.ReadLine();
+int expFirst = 0;
+int expSecond =0;
+int[] workArr = new int[24];
+
+workArr = MyLib.giveFullIntArray(3, 24);
+for (int i = 0; i < workArr .Length; i++)
 {
-    if ((expOne[i] % 2) == 0) {multiple = multiple + expOne[i];}
-    if ((expOne[i] % 2) != 0) {noMultiple = noMultiple + expOne[i];}
+    if ((workArr[i] % 2) == 0) {expFirst = expFirst + workArr[i];}
 }
-System.Console.WriteLine($" сумма кратных чисел в массиве {multiple}");
-System.Console.WriteLine($" сумма  не кратных чисел в массиве {noMultiple}");
+System.Console.WriteLine($" сумма чётных чисел в массиве {expFirst}");
+System.Console.WriteLine();
+System.Console.WriteLine("Вторая задача. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях. Нажмите ENTER ");
+System.Console.ReadLine();
+for (int i = 0; i < workArr.Length ; i++)
+{
+    if ((i%2)==0){expSecond= expSecond + workArr[i];} 
+}
+System.Console.WriteLine($" сумма чисел на чётных позициях в массиве равна {expSecond}");
+System.Console.WriteLine();
+
