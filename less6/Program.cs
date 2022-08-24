27423ae131cef255b90b7c64bf8a26d5d1963231
zhukovv <northbug@yandex.ru>
2022-08-24 14:16:16 +0500

    solved first task for sixth lesson

---
+++ less6/Program.cs
@@ -0,0 +1,6 @@
+﻿
+System.Console.WriteLine("Задача 1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Нажмите ENTER ");
+Console.ReadLine();
+System.Console.WriteLine("Введите значение М");
+int M = Convert .ToInt32(Console.ReadLine());
+System.Console.WriteLine($" Чисел больше нуля было введено {MyLib .countMyNumGreaterZero(M)}");
