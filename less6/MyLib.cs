e79043a2be0adace2b9c205b55f26a803ed3dfcb
zhukovv <northbug@yandex.ru>
2022-08-25 10:40:11 +0500

    solved second task sixth lesson

---
+++ less6/MyLib.cs
@@ -0,0 +1,200 @@
+using System.Text;
+public static class MyLib
+{
+
+    public static int numToStep(int n, int step)
+    {
+        int incr = n;
+
+        for (int h = 1; h != step; h++)
+        {
+            n = n * incr;
+        }
+        return n;
+    }
+
+    public static int getIndexes(int num)
+    {
+        int digit = 10;
+        int stp = 1;
+
+
+        while (num > digit)
+        {
+            stp++;
+            digit = numToStep(10, stp);
+        }
+        return stp;
+    }
+
+    public static int[] intToArray(int num, int count)
+    {
+        int i, x = 0, y = 0;
+        int[] arr = new int[count];
+        for (i = (count - 1); i >= 0; i--)
+        {
+            x++;
+
+            if (i == arr.Length - 1)
+            {
+                arr[i] = num % (numToStep(10, x));
+            }
+            else
+            {
+                y++;
+                arr[i] = (num / (numToStep(10, y))) % 10;
+            }
+
+        }
+        return arr;
+    }
+
+    public static int giveMeSummArr(int[] arr)
+    {
+        int summ = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            summ = summ + arr[i];
+        }
+        return summ;
+    }
+
+    public static void showMeYourArrrr()
+    {
+        int[] arr = new int[8];
+        Random rnd = new Random();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rnd.Next(1, 101);
+            System.Console.Write(arr[i] + " ");
+        }
+    }
+
+    public static int[] giveMeBinArray(int num)
+    {
+        int[] arr = new int[num];
+        Random ran = new Random();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = ran.Next(0, 2);
+            System.Console.Write(arr[i] + " ");
+        }
+        return arr;
+
+
+    }
+
+    public static int giveMeDigitalNum(string strBinNum)
+    {
+        int digNum = 0;
+        int ind = strBinNum.Length;
+
+        for (int i = 0; i < ind; i++)
+        {
+
+            if ((Convert.ToInt32(strBinNum[i]) == 49))
+            {
+                digNum = digNum + numToStep(2, (ind - i));
+            }
+
+        }
+        return digNum;
+    }
+
+    // public static string arrayToString(string [] arr)
+    // {
+    //     StringBuilder str = 
+    //     for(int i=0; i < arr.Length ; i++)
+    //     {
+    //         System.Console.Write(arr[i]);
+    //     }
+    // }
+
+
+    public static void giveMeNumSys(string digNum, int ss)
+    {
+
+        string[] arr = new string[32];
+        int workNum = Convert.ToInt32(digNum);
+        int ind = 0;
+
+        for (int i = 0; workNum > 0; i++)
+        {
+            arr[i] = Convert.ToString(workNum % ss);
+            workNum = workNum / ss;
+            ind = i;
+        }
+
+        Array.Resize(ref arr, ind + 1);
+        Array.Reverse(arr);
+        for (int i = 0; i < arr.Length; i++)
+        {
+            System.Console.Write(arr[i]);
+        }
+        //     StringBuilder str = new();
+        //     for (int i = 0; i < arr.Length ; i++)
+        //     {
+        //         str[i]=Convert .ToChar(arr[i]);
+        //     }
+        // return str;
+    }
+
+    public static int[] giveFullIntArray(int numSize, int arrLength)
+    {
+        int[] workArr = new int[arrLength];
+        Random rand = new Random();
+        int min = numToStep(10, numSize - 1);
+        int max = numToStep(10, numSize);
+
+        for (int i = 0; i < arrLength; i++)
+        {
+            workArr[i] = rand.Next(min, max);
+        }
+        return workArr;
+    }
+    public static (double min, double max) sortDoubleArr(int size)
+    {
+        double[] arr = new double[size];
+        Random rand = new Random();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rand.NextDouble();
+        }
+        double max = arr[1];
+        double min = arr[1];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] > max) { max = arr[i]; }
+            if (arr[i] < min) { min = arr[i]; }
+        }
+        return (min, max);
+
+    }
+
+
+    public static int countMyNumGreaterZero(int M)
+    {
+        int workNum = 0;
+        int count = 0;
+        for (int i = 0; i < M; i++)
+        {
+            System.Console.WriteLine("Введите число ");
+            workNum = Convert.ToInt32(Console.ReadLine());
+            if (workNum > 0) { count++; }
+        }
+        return count;
+    }
+
+    public static (double x, double y, double [] arr) pointIntersectionLines(double k1, double k2, double b1 , double b2)
+    {
+        double x = (b2-b1)/(k1-k2);
+        double y = (k1*x)+b1;
+        double [] arr = new double[2];
+        arr[0] = x;
+        arr[1] = y;
+        return (x,y, arr);
+    }
+
+}
+
--- less6/Program.cs
+++ less6/Program.cs
@@ -1,6 +1,22 @@
 ﻿
-System.Console.WriteLine("Задача 1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Нажмите ENTER ");
+// System.Console.WriteLine("Задача 1. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Нажмите ENTER ");
+// Console.ReadLine();
+// System.Console.WriteLine("Введите значение М");
+// int M = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($" Чисел больше нуля было введено {MyLib.countMyNumGreaterZero(M)}");
+///////////////////////////////////////////////////////////////////////////////////////////////
+System.Console.WriteLine("Задача 2. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями. Нажмите ENTER ");
 Console.ReadLine();
-System.Console.WriteLine("Введите значение М");
-int M = Convert .ToInt32(Console.ReadLine());
-System.Console.WriteLine($" Чисел больше нуля было введено {MyLib .countMyNumGreaterZero(M)}");
+System.Console.WriteLine("Прямая задаётся формулой y=kx+b, задайте значения для k и b");
+System.Console.WriteLine(" значение для k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(" значение для b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(" значение для k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine(" значение для b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+double [] arrPoint = new double [2];
+
+arrPoint = MyLib.pointIntersectionLines(k1, k2, b1, b2).arr;
+System.Console.WriteLine($"значение x = {MyLib.pointIntersectionLines(k1, k2, b1, b2).x} значение y = {MyLib.pointIntersectionLines(k1, k2, b1, b2).y}");
