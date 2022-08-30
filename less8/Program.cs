be50f06a2115a7bed5d33350ade518a601c78fb5
zhukovv <northbug@yandex.ru>
2022-08-30 16:56:16 +0500

    start work for second task

--- less8/Program.cs
+++ less8/Program.cs
@@ -16,3 +16,18 @@ System.Console.WriteLine();
 System.Console.WriteLine();
 Console.WriteLine("Строки массива отстортированы . Нажмите ENTER");
 Console.ReadLine();
+
+Console.WriteLine("Задача 2.Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов. Нажмите ENTER");
+Console.ReadLine();
+System.Console.WriteLine();
+double [] arrSumm = new double[exmArr .GetLength(0)];
+for (int i = 0; i < exmArr.GetLength(0); i++)
+{
+    double lineSum = 0;
+    for (int j = 0; j < exmArr.GetLength(1); j++)
+    {
+        lineSum =lineSum +exmArr[i,j];
+    }
+    arrSumm[i] = lineSum ;
+    System.Console.WriteLine($"Строка с наименьшей суммой   {MyLib.giveMeMinMaxDoubleArr(arrSumm).min}");
+}
