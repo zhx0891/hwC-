9b04d48775b0ccaddd34dc215ed3f5b9ca1a1bfa
zhukovv <northbug@yandex.ru>
2022-08-30 16:27:36 +0500

    solved first task for eighth less

---
+++ less8/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Задача 1. Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива. Нажмите ENTER");
+Console.ReadLine();
+System.Console.WriteLine();
+
+double[,] exmArr = new double[5, 5];
+
+exmArr = MyLib.giveMeFullTwoDimensionalArray(5, 5);
+System.Console.WriteLine();System.Console.WriteLine();
+
+Console.WriteLine("Массив создан. Нажмите ENTER");
+Console.ReadLine();
+System.Console.WriteLine();
+
+MyLib.sortLineDoubleArrayUptoDown(exmArr);
+System.Console.WriteLine();
+System.Console.WriteLine();
+Console.WriteLine("Строки массива отстортированы . Нажмите ENTER");
+Console.ReadLine();
