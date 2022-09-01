5aa17311b15d1a79dfd3ed79cc79e5a1d95e0e6a
zhukovv <northbug@yandex.ru>
2022-09-01 16:55:33 +0500

    solved **task for eighth lesson

--- less8/MyLib.cs
+++ less8/MyLib.cs
@@ -323,12 +323,15 @@ public static class MyLib
         else
         {
             
-            for (int i = 0; i < resArr.GetLength(0); i++)  //строка первой матрицы
-            {
+            for (int i = 0; i < resArr.GetLength(0); i++)  
+            {   
+
                 System.Console.WriteLine();
-                for (int j = 0; j < resArr .GetLength(1); i++)
+                for (int j = 0; j < resArr .GetLength(1); j++)
                 {
-                    resArr=(arr[i,0]*arr1[0,j])+(arr[i,1]*arr1[1,j]);
+                    resArr[i,j]=(arr[i,0]*arr1[0,j]) + (arr[i,1]*arr1[1,j]);
+                    System.Console.Write(resArr[i,j] + " " );
+                    
                 }
             }
         }
