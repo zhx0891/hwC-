92795083fe8b95ea8f5b0dce2e67e171a3fe9ec0
zhukovv <northbug@yandex.ru>
2022-09-13 16:39:42 +0500

    ex50

--- ex/Program.cs
+++ ex/Program.cs
@@ -1,14 +1,27 @@
 ﻿// 39. Найти произведение пар чисел в одномерном массиве.
-int [] arr = new int [10];
-for (int i = 0; i < arr. Length; i++)
+// int [] arr = new int [10];
+// for (int i = 0; i < arr. Length; i++)
+// {
+//    arr[i]=i;
+//   System.Console.Write(arr[i]+ " ");
+// }
+// System.Console.ReadLine();
+// int leng =arr.Length-1;
+// for (int i = 0; i < (arr.Length)/2; i++)
+// {
+//      System.Console.Write($"{arr[i]  * arr[leng-i]} ");
+//      System.Console.ReadLine();
+// }
+
+// Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int [,] arr = new int [5,5];
+for (int i = 0; i < arr.GetLength(0); i++)
+{ System.Console.WriteLine();
+    for (int j = 0; j < arr.GetLength(1); j++)
+    
 {
-   arr[i]=i;
-  System.Console.Write(arr[i]+ " ");
-}
-System.Console.ReadLine();
-int leng =arr.Length-1;
-for (int i = 0; i < (arr.Length)/2; i++)
-{
-     System.Console.Write($"{arr[i]  * arr[leng-i]} ");
-     System.Console.ReadLine();
+        arr[i,j] = i+j;
+        System.Console.Write(arr[i,j]+" ");
+    }
 }
