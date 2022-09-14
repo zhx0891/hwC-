44efd38e49ba4aba38bf7c1ed89a8d0389cf094f
zhukovv <northbug@yandex.ru>
2022-09-14 14:46:38 +0500

    написан метод конвертации двоичного числа в десятичное

---
+++ ConvertBitoDigit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+System.Console.WriteLine("Введите двоичное число");
+string num = "1010101";
+int [] biNum = new int [num.Length];
+int leng=biNum.Length;
+for (int i = 0; i < num.Length; i++)
+{
+    if((num[i].Equals('0'))||(num[i].Equals('1')))
+    {
+        biNum[(leng-i)-1]=(num[i]-'0');
+    }
+    else
+    {
+        System.Console.WriteLine("это не двоичное число");
+    }
+}
+
+for (int i = 0; i < biNum.Length; i++)
+{
+    System.Console.Write(biNum[i] + " ");
+}
+int digNum=0;
+for (int i = 0; i <biNum.Length; i++)
+{
+    int step =1;
+    for (int j = 0; j < i; j++)
+    {
+    step=step*2;
+    }
+    
+    digNum=digNum+(biNum[i]*step);
+}
+System.Console.WriteLine(digNum);
--- ex/Program.cs
+++ ex/Program.cs
@@ -15,13 +15,14 @@
 
 // Задать двумерный массив следующим правилом: Aₘₙ = m+n
 
-int [,] arr = new int [5,5];
-for (int i = 0; i < arr.GetLength(0); i++)
-{ System.Console.WriteLine();
-    for (int j = 0; j < arr.GetLength(1); j++)
-    
-{
-        arr[i,j] = i+j;
-        System.Console.Write(arr[i,j]+" ");
-    }
-}
+// int[,] arr = new int[5, 5];
+
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     System.Console.WriteLine();
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i, j] = i + j;
+//         System.Console.Write(arr[i, j] + " ");
+//     }
+// }
