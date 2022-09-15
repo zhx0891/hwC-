688f3af7161418b7c2ce32c7a1c043b4a2bdc395
zhukovv <northbug@yandex.ru>
2022-09-15 10:52:45 +0500

    added FrequencyDictionary project

---
+++ FrequencyDictionary/Program.cs
@@ -0,0 +1,11 @@
+﻿var rand = new Random();
+int [,] arr = new int [5,5];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    System.Console.WriteLine();
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i,j]=rand .Next(0,10);
+        System.Console.Write(arr[i,j]+ " ");
+    }
+}
