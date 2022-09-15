9529c51c46ae0bf80d82aa09e31f8f60dbf83eb6
zhukovv <northbug@yandex.ru>
2022-09-15 11:43:58 +0500

    I do it

--- manipulateArray/Program.cs
+++ manipulateArray/Program.cs
@@ -3,10 +3,12 @@ int[,] arr = new int[5, 5];
 arr=Elements .giveMeFullArr();
 
 int  lastString = arr.GetLength(0)-1;
+
 for (int i = 0; i < arr.GetLength(1); i++)
 {
     temp=arr[lastString,i];
     arr[lastString,i]=arr[0,i];
     arr[0,i]=temp;
 }
+System.Console.WriteLine();
 Elements .showArr(arr);
