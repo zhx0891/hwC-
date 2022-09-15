7eae734be7c58eb51580721a48251ded4e36a6de
zhukovv <northbug@yandex.ru>
2022-09-15 11:58:57 +0500

    I do it again

--- manipulateArray/Elements.cs
+++ manipulateArray/Elements.cs
@@ -18,13 +18,36 @@ public static class Elements
 
     public static void showArr(int[,] arr)
     {
-for (int i = 0; i < arr .GetLength(0); i++)
-{
-    System.Console.WriteLine();
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-        System.Console.Write(arr[i,j] + " ");
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            System.Console.WriteLine();
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                System.Console.Write(arr[i, j] + " ");
+            }
+        }
     }
-}
+
+    public static int [,] replaceStrToRows(int [,] arr)
+    {
+        int[,] arr2 = new int [arr.GetLength(0),arr.GetLength(1)]; 
+        if(arr.GetLength(0)==arr.GetLength(1))
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr2[j,i]=arr[i,j];
+                }
+            }
+            
+            
+            return arr2 ;
+        }
+        else
+        {
+            System.Console.WriteLine(" этот массив невозможно переверуть");
+        }
+        return arr;
     }
 }
--- manipulateArray/Program.cs
+++ manipulateArray/Program.cs
@@ -1,14 +1,17 @@
-﻿int temp = 0;
+﻿// Меняем первую и последнюю строку местами.
+int temp = 0;
 int[,] arr = new int[5, 5];
 arr=Elements .giveMeFullArr();
-
 int  lastString = arr.GetLength(0)-1;
-
 for (int i = 0; i < arr.GetLength(1); i++)
 {
     temp=arr[lastString,i];
     arr[lastString,i]=arr[0,i];
     arr[0,i]=temp;
 }
+System.Console.WriteLine();
+Elements .showArr(arr);
+//Меняем строки на столбцы.
+arr=Elements .replaceStrToRows(arr);
 System.Console.WriteLine();
 Elements .showArr(arr);
