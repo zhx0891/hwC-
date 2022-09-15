4ed0c3a49c567a89de06e806661ea94770849b9f
zhukovv <northbug@yandex.ru>
2022-09-15 11:09:16 +0500

    частотный словарь решено

--- FrequencyDictionary/Program.cs
+++ FrequencyDictionary/Program.cs
@@ -1,11 +1,25 @@
 ﻿var rand = new Random();
-int [,] arr = new int [5,5];
+int[] dict = new int[10];
+int[,] arr = new int[5, 5];
 for (int i = 0; i < arr.GetLength(0); i++)
 {
     System.Console.WriteLine();
     for (int j = 0; j < arr.GetLength(1); j++)
     {
-        arr[i,j]=rand .Next(0,10);
-        System.Console.Write(arr[i,j]+ " ");
+        arr[i, j] = rand.Next(0, 10);
+        System.Console.Write(arr[i, j] + " ");
     }
+}
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        dict[arr[i, j]] =dict[arr[i, j]] +1;
+    }
+}
+System.Console.WriteLine();
+
+for (int i = 0; i < dict.Length; i++)
+{
+    System.Console.WriteLine($"Число {i} встречается  {dict[i]}  раз ");
 }
